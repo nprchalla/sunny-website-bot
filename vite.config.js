9d28c669e4b1130d4271853dd406fcb4f49c893f
“[Naga <[bunnyakhi1623@gmail.com]>
2026-03-09 14:09:30 -0400

    Add voip-office page to Vite build input

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        contact: resolve(__dirname, 'contact.html'),
-        chatbot: resolve(__dirname, 'chatbot.html'),
-        news: resolve(__dirname, 'news.html'),
+        voipOffice: resolve(__dirname, 'voip-office.html'),
         wayneState: resolve(__dirname, 'wayne-state.html'),
-        michiganGop: resolve(__dirname, 'michigan-gop.html')
+        michiganGop: resolve(__dirname, 'michigan-gop.html'),
+        news: resolve(__dirname, 'news.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        chatbot: resolve(__dirname, 'chatbot.html')
       }
     }
   }
